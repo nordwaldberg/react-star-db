68c9159f8658f3693abe2eb22ffeab6b1730bd57
nord-waldberg <nord.waldberg@gmail.com>
2020-09-28 14:28:39 +0300

    Remove build in .gitignore

---
+++ build/precache-manifest.2b75d74210d12bf4e6894c59000a1541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ce48c1a85ef543b96da6207b66f1a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3173bef4135fd9f318a",
+    "url": "/static/css/main.93d5f363.chunk.css"
+  },
+  {
+    "revision": "ed4990f8fd534ac619eb",
+    "url": "/static/js/2.7fd45f73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fd45f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3173bef4135fd9f318a",
+    "url": "/static/js/main.d17ea0ef.chunk.js"
+  },
+  {
+    "revision": "449c9ad6839f1072e88b",
+    "url": "/static/js/runtime-main.59864514.js"
+  }
+]);
